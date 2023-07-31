b5d2fd1fe6ee5956cbce3a6bb9a92f2efbdb2d41
BuildTools <unconfigured@null.spigotmc.org>
2023-07-31 16:50:28 +0800

    test commit

---
+++ src/main/java/com/maanraj514/BridgePlugin.java
@@ -0,0 +1,17 @@
+package com.maanraj514;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BridgePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
