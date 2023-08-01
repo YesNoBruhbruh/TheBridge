daa1e2eae78beea148469a93097ebe35c314b78f
BuildTools <unconfigured@null.spigotmc.org>
2023-08-01 20:45:00 +0800

    last commit of the day

--- src/main/java/com/maanraj514/BridgePlugin.java
+++ src/main/java/com/maanraj514/BridgePlugin.java
@@ -1,10 +1,14 @@
 package com.maanraj514;
 
+import com.maanraj514.game.GameManager;
+
 public final class BridgePlugin extends Okmeta {
 
     @Override
     public void enable() {
         saveDefaultConfig();
+
+        new GameManager(this);
     }
 
     @Override
--- src/main/java/com/maanraj514/game/GameManager.java
+++ src/main/java/com/maanraj514/game/GameManager.java
@@ -1,20 +1,25 @@
 package com.maanraj514.game;
 
+import com.maanraj514.BridgePlugin;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class GameManager {
 
+    private final BridgePlugin plugin;
+
     private final Map<UUID, Game> games;
 
-    public GameManager(){
+    public GameManager(BridgePlugin plugin){
+        this.plugin = plugin;
         this.games = new HashMap<>();
     }
 
     public void createGame(String map, GameMode mode){
         UUID uuid = UUID.randomUUID();
-        this.games.put(uuid, new Game(map, mode, uuid));
+        this.games.put(uuid, new Game(map, mode, uuid, plugin));
     }
 
     public void deleteGame(UUID uuid){
